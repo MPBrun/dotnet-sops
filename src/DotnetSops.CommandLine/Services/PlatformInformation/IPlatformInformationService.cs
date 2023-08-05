f6b4ad6bb3f964c8fe8e5c63e9f64535bfc80c10
Peter Sandberg Brun <peter@mpbrun.dk>
2023-08-05 14:04:02 +0200

    Made the SOPS executable on macOS and Linux (#2)

--- src/DotnetSops.CommandLine/Services/PlatformInformation/IPlatformInformationService.cs
+++ src/DotnetSops.CommandLine/Services/PlatformInformation/IPlatformInformationService.cs
@@ -1,13 +1,17 @@
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 namespace DotnetSops.CommandLine.Services.PlatformInformation;
 internal interface IPlatformInformationService
 {
     Architecture ProcessArchitecture { get; }
 
+    [SupportedOSPlatformGuard("macOS")]
     bool IsMacOS();
 
+    [SupportedOSPlatformGuard("linux")]
     bool IsLinux();
 
+    [SupportedOSPlatformGuard("windows")]
     bool IsWindows();
 }
